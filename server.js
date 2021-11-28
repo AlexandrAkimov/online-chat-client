e42e63bdbc7313b1214ccccdddca11331faf57f1
AlexandrAkimov <akimovy14072018@yandex.ru>
2021-11-28 22:38:56 +0300

    deploy

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.static('build'));
-
+//app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () =>
