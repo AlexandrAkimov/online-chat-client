e8a4cb5c82c8b2e4edb2866795588f4083580259
AlexandrAkimov <akimovy14072018@yandex.ru>
2021-11-29 08:19:26 +0300

    deploy

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-app.use(express.static('build'));
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port, () =>
   console.log(`Server is running on: http://localhost:${port}`));
